d1603c457bf3d31fd74fd00ff13bc63f93f6b4ed
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-03-12 17:22:23 +0800

    asdas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,18 @@ export default function Home() {
 		<div className="body">
 			<div className="navbar">
 				<h2>Pokedex</h2>
+				<div>Menu</div>
 			</div>
-			<div className="card">
-				<img src="https://png.pngtree.com/png-vector/20240322/ourmid/pngtree-capybara-photograph-as-a-funny-way-to-illustrate-nature-photographer-png-image_12192166.png"></img>
-				<div className="bio">
-					<h1>Catch 'Em All!</h1>
-					<p>Explore with Our Interactive Pokedex</p>
-					<button>View Pokedex</button>
+			<div className="section">
+				<div className="card">
+					<img src="https://png.pngtree.com/png-vector/20240322/ourmid/pngtree-capybara-photograph-as-a-funny-way-to-illustrate-nature-photographer-png-image_12192166.png"></img>
+					<div className="bio">
+						<div className="content">
+							<h1>Catch 'Em All!</h1>
+							<p>Explore with Our Interactive Pokedex</p>
+						</div>
+						<button>View Pokedex</button>
+					</div>
 				</div>
 			</div>
 		</div>
